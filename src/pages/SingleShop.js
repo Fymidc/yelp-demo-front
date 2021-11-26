7e8eaa00d10ac5b6d240e61aaa3a3682af1efc6d
unknown <bodurfatih1967@gmail.com>
2021-11-26 17:57:38 +0300

    edited

--- src/pages/SingleShop.js
+++ src/pages/SingleShop.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import ButtonBase from '@mui/material/ButtonBase';
+import { Button, Divider, Link } from '@mui/material';
+import StarIcon from '@mui/icons-material/StarBorderOutlined';
+import LikeIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import BookmarkIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import CallIcon from '@mui/icons-material/CallEndOutlined';
+import DirectionIcon from '@mui/icons-material/DirectionsOutlined';
+import LanguageIcon from '@mui/icons-material/LanguageOutlined';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Comment from '../layouts/Comment';
 
 function SingleShop() {
     return (
@@ -17,6 +25,63 @@ function SingleShop() {
                 </div>
                 <div className="single-shop-cart" >
                     <SingleShopBox />
+                </div>
+
+            </div>
+
+            <div >
+                <div style={{ marginLeft: "5rem", textAlign: "left" }} >
+                    <Button style={{ marginTop: "1rem", marginRight: "1rem" }} color="warning" startIcon={<StarIcon />} variant="outlined">Write a Review</Button>
+                    <Button style={{ marginTop: "1rem", marginRight: "1rem" }} color="warning" startIcon={<LikeIcon />} variant="outlined">Like</Button>
+                    <Button style={{ marginTop: "1rem", marginRight: "1rem" }} color="warning" startIcon={<BookmarkIcon />} variant="outlined">Save</Button>
+
+                </div>
+            </div>
+
+            <div style={{ marginTop: "2rem", margin: "2rem 5rem", justifyContent: "space-between", height: "50rem", display: "flex" }} >
+                <div style={{ width: "67%" }}>
+                    <div >
+                        <Divider variant="middle" />
+                        <Typography sx={{ padding: "1rem" }} >
+                            Menu
+                        </Typography>
+                        <Button style={{ marginBottom: "1rem", marginRight: "1rem" }} color="warning" startIcon={<LanguageIcon />} variant="outlined">Website Menu</Button>
+                        <Divider variant="middle" />
+                    </div>
+
+
+
+                    <div>
+                        <Comment />
+                        <Divider />
+                        <Comment />
+                        <Divider />
+                    </div>
+
+
+                </div>
+
+                <div style={{ position: "sticky", top: 0, zIndex: 2 }} >
+                    <Card sx={{ border: "1px solid #ebe4c6", maxWidth: 405, textAlign: "left" }}>
+                        <CardContent>
+                            <Typography sx={{ padding: "1rem", fontSize: 16, position: "relative" }} color="body1" startIcon={<StarIcon />} gutterBottom>
+                                (0212)654 65 65 <CallIcon style={{ right: "0", fontSize: "inherit", position: 'absolute', top: '15px' }} />
+                            </Typography>
+                            <Divider />
+                            <Typography sx={{ padding: "1rem", mb: 1.5, position: "relative" }} color="body1"  >
+                                Suadiye Mah. Bağdat Cad. Mücahit Sok. No:5 34732 Istanbul Turkey
+                                <DirectionIcon style={{ right: "0", fontSize: "inherit", position: 'absolute', top: '15px' }} />
+                            </Typography>
+                            <Divider />
+                            <Typography sx={{ padding: "1rem", position: "relative" }} variant="body2">
+                                <Link  href="#" underline="none">
+                                    {'http://www.olearys.com.tr'}
+                                </Link>
+                                <LanguageIcon style={{ right: "0", fontSize: "inherit", position: 'absolute', top: '15px' }} />
+                            </Typography>
+                        </CardContent>
+
+                    </Card>
                 </div>
 
             </div>
@@ -39,22 +104,22 @@ export default SingleShop
 
 function SingleShopBox() {
     return (
-        <Paper sx={{ boxShadow:"none", color:"white" ,backgroundColor:"transparent", p: 2, padding: "2rem", margin: 'auto', marginTop: "11rem", maxWidth: 500, flexGrow: 1 }}>
+        <Paper sx={{ boxShadow: "none", color: "white", backgroundColor: "transparent", p: 2, padding: "2rem", margin: 'auto', marginTop: "11rem", maxWidth: 500, flexGrow: 1 }}>
             <Grid container spacing={2}>
 
-                <Grid item xs={12} xs container sx={{ fontWeight:"bold", textAlign: "left" }} >
+                <Grid item xs={12} xs container sx={{ fontWeight: "bold", textAlign: "left" }} >
                     <Grid item xs container direction="column" spacing={2}>
-                        <Grid  item xs>
-                            <Typography sx={{ fontWeight:"bold"}} gutterBottom variant="h3" component="div">
+                        <Grid item xs>
+                            <Typography sx={{ fontWeight: "bold" }} gutterBottom variant="h3" component="div">
                                 Cafe Name  My Cafe
                             </Typography>
-                            <Typography sx={{ fontWeight:"bold"}}variant="body1" component="div">
+                            <Typography sx={{ fontWeight: "bold" }} variant="body1" component="div">
                                 10/LİKE
                             </Typography>
-                            <Typography sx={{fontWeight:"bold", cursor: 'pointer' }} variant="body1" gutterBottom>
+                            <Typography sx={{ fontWeight: "bold", cursor: 'pointer' }} variant="body1" gutterBottom>
                                 Cafe info-(tea etc.)
                             </Typography>
-                            <Typography sx={{ fontWeight:"bold"}} variant="body1" >
+                            <Typography sx={{ fontWeight: "bold" }} variant="body1" >
                                 isOpen!
                             </Typography>
                         </Grid>
